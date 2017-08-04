2d3e0481ae8c03ded6afa73c9535ebdddb1ff1ca
Benoit Petit <bpetit@b0rk.in>
2017-08-04 18:15:08 +0200

    Added module netbox_facts.

---
+++ netbox_facts.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+from ansible.module_utils.basic import AnsibleModule
+from netboxapi_client import Api, get_list, get
+import json
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            name      = dict(required=False),
+            ident     = dict(required=False),
+            model     = dict(required=True),
+            obj       = dict(required=True),
+            token     = dict(required=True),
+            url       = dict(required=True)
+        ),
+        # supports_check_mode=True,
+        mutually_exclusive = [ ('name', 'ident') ]
+    )
+
+    # we initialize the Api object
+    api = Api(
+        url=module.params['url'],
+        token=module.params['token']
+    )
+
+    ansible_facts = dict()
+
+    # if no name or id is specified, we enumerate objects
+    if not module.params['name'] and not module.params['ident']:
+        res = get_list(api, model=module.params['model'], obj=module.params['obj'])
+        ansible_facts['netbox_result'] = res
+
+    if module.params['name'] and not module.params['ident']:
+        res = get(api, model=module.params['model'], obj=module.params['obj'], name=module.params['name'])
+        ansible_facts['netbox_result'] = res
+
+    if not module.params['name'] and module.params['ident']:
+        res = get(api, model=module.params['model'], obj=module.params['obj'], ident=module.params['ident'])
+        ansible_facts['netbox_result'] = res
+
+    module.exit_json(changed=True, result=res, ansible_facts=ansible_facts)
+
+if __name__ == '__main__':
+    main()
