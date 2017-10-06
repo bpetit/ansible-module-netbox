61068090f66f454d6bb3b39ac3ae554f545efa05
Benoit Petit <bpetit@0rk.in>
2017-10-06 18:15:41 +0200

    Added data option to netbox module

--- netbox.py
+++ netbox.py
@@ -8,16 +8,18 @@ import json
 from os import path
 
 def json_are_the_same(first, second):
+    res = False
     for k, v in first.items():
         if k not in second:
             return False
         else:
             if v is not dict:
-                if not v == second[k]:
+                if not first[k] == second[k]:
                     return False
-                return True
+                res = True
             else:
-                json_are_the_same(first[k], second[k])
+                 json_are_the_same(first[k], second[k])
+    return res
 
 def main():
     module = AnsibleModule(
@@ -29,7 +31,8 @@ def main():
             token     = dict(required=True),
             url       = dict(required=True),
             state     = dict(required=True, choices=['present', 'absent']),
-            template  = dict(required=False)
+            template  = dict(required=False),
+            data      = dict(required=False)
         ),
         # supports_check_mode=True,
         mutually_exclusive = [ ('name', 'ident') ]
@@ -41,6 +44,7 @@ def main():
     )
 
     changed = False
+    failed = False
 
     res = {}
 
@@ -52,32 +56,43 @@ def main():
     )
 
     # state = present
-    if module.params['state'] == 'present' and 'template' in module.params:
-        # get current object
-        # get the data from the template
-        template = module.params['template']
-        env = Environment(
-            loader=FileSystemLoader(path.dirname(template))
-        )
-        template = env.get_template(path.basename(template))
-        data = json.loads(template.render().replace("'", "\""))
-        res = data
+    if module.params['state'] == 'present':
+        if 'template' in module.params and module.params['template'] is not None:
+            # get current object
+            # get the data from the template
+            template = module.params['template']
+            env = Environment(
+                loader=FileSystemLoader(path.dirname(template))
+            )
+            template = env.get_template(path.basename(template))
+            data = json.loads(template.render().replace("'", "\""))
+        elif 'data' in module.params:
+            data = json.loads(module.params['data'].replace("'", "\""))
+
         # if current object data and required object data are the same
-        if not json_are_the_same(current, res):
+        if not json_are_the_same(data, current): #FIX
             if 'detail' in current and current['detail'] == 'Not found.':
                  res = create(api, model=module.params['model'], obj=module.params['obj'], name=module.params['name'], ident=module.params['ident'], data=data)
             else:
                  res = update(api, model=module.params['model'], obj=module.params['obj'], name=module.params['name'], ident=module.params['ident'], data=data)
-            if 200 <= res.status_code <=299:
-                # the request suceeded
-                 changed=True
 
-        result = res.json()
+            # if update failed because of already used slug or name
+            if "already exists" in res['name'][0] or "already exists" in res['slug'][0]:
+                changed=False
+                failed=True
+            else:
+                changed=True
+
+            result = res
+        else:
+            result = "Nothing changed."
     elif module.params['state'] == 'absent':
         res = delete(api, model=module.params['model'], obj=module.params['obj'], name=module.params['name'], ident=module.params['ident'])
-        result = res.json()
+        result = res
+    else:
+        raise Exception
 
-    module.exit_json(changed=changed, result=result)
+    module.exit_json(changed=changed, result=result, failed=failed)
 
 if __name__ == '__main__':
     main()
